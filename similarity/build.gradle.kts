315cbd68be5ba24681772afc68ecb8a736deb3d8
Armin Zavada <zavadaarmin@edu.bme.hu>
2024-10-18 09:49:25 +0200

    Set Node to download v22.0.0

--- similarity/build.gradle.kts
+++ similarity/build.gradle.kts
@@ -7,6 +7,11 @@ plugins {
     id("com.github.node-gradle.node") version "7.1.0"
 }
 
+node {
+    download.set(true)
+    version.set("22.0.0")
+}
+
 application {
     mainClass = "hu.bme.mit.ase.shingler.similarity.SimilarityApp"
 }
