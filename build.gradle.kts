4e441ec7c6d2b15a9d85ad8e0546a6563581a97b
Armin Zavada <zavadaarmin@edu.bme.hu>
2025-11-01 19:22:30 +0100

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,9 @@ repositories {
 
 dependencies {
     implementation(refinery.generator)
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.12.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.4")
 }
 
 java.toolchain {
