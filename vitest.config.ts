faff1e6a78a7c879d39b480f2ff5c4219ff71363
Zoltan Micskei <micskeiz@mit.bme.hu>
2026-05-11 11:20:41 +0200

    Migrate vitest workspaces to projects

---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        test: {
+          name: "unit",
+          include: ["**/*.unit.test.ts"],
+        },
+      },
+      {
+        test: {
+          name: "integration",
+          include: ["**/*.integration.test.ts"],
+        },
+      },
+      {
+        test: {
+          name: "e2e",
+          include: ["**/*.e2e.test.ts"],
+        },
+      },      
+    ],
+  },
+});
